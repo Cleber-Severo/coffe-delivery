8bcdcfc709fe4802dcfca112647f61622b885acc
Cléber Severo <clebersevero1998@gmail.com>
2025-05-04 21:04:58 -0300

    feat: theme configuring

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 02px ${(props) => props.theme['yellow-dark']}
+  }
+
+  body {
+    background: ${(props) => props.theme['backgorund']};
+    color: ${(props) => props.theme['base-label']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textArea, button {
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+`;
