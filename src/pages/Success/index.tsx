2e13de729b800c06e43193b895c68dd7b4338859
Cleber Severo <clebersevero1998@gmail.com>
2025-05-18 22:12:58 -0300

    feat: success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,12 +1,65 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
+import { Card, Container, IconWrapper, Item, SubTitle, Title } from "./styles";
+import CarImgSrc from '../../assets/Illustration.png'
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+import { defaultTheme } from "../../styles/themes/default";
+
+type PaymentMethod = 'credito' | 'debito' | 'dinheiro';
+
+const paymentsMap: Record<PaymentMethod, string> = {
+  credito: 'Cartão de crédito',
+  debito: 'Cartão de débito',
+  dinheiro: 'dinheiro',
+};
 
 const Success = () => {
-    const { orderDetails } = useContext(CartContext)
-    console.log(" Success ~ orderDetails:", orderDetails)
-  
-  
-	return <div>Success</div>;
+  const { orderDetails } = useContext(CartContext)
+  console.log(" Success ~ orderDetails:", orderDetails)
+
+  return <Container>
+    <div>
+      <Title>Uhuu! Pedido Confirmado</Title>
+      <SubTitle>Agora é só aguardar que logo o café chegará até você</SubTitle>
+      <Card>
+        <div>
+          <Item>
+            <IconWrapper style={{ backgroundColor: defaultTheme.purple }}>
+              <MapPin size={16} weight="fill" color="#FFF" />
+            </IconWrapper>
+            <div>
+              <span>Entrega em: <strong>{orderDetails.rua}, {orderDetails.numero}</strong></span>
+              <span>{orderDetails.bairro} - {orderDetails.cidade}, {orderDetails.uf}</span>
+            </div>
+          </Item>
+          <Item>
+            <IconWrapper style={{ backgroundColor: defaultTheme["yellow"] }}>
+              <Timer size={16} weight="fill" color="#FFF" />
+            </IconWrapper>
+            <div>
+              <span>Pagamento de entrega</span>
+              <span><strong>20min - 30 min</strong></span>
+            </div>
+          </Item>
+          <Item>
+            <IconWrapper style={{ backgroundColor: defaultTheme["yellow-dark"] }}>
+              <CurrencyDollar  size={16} color="#FFF" />
+            </IconWrapper>
+            <div>
+              <span>Previsão na entrega</span>
+              <span>
+                <strong>
+                  {paymentsMap[orderDetails.paymentMethod as PaymentMethod]}
+                </strong>
+              </span>
+            </div>
+          </Item>
+        </div>
+      </Card>
+
+    </div>
+    <img src={CarImgSrc} alt="" />
+  </Container>;
 };
 
 export default Success;
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+
+	padding: 80px 160px;
+	width: 1440px;
+	margin: 0 auto;
+`;
+
+
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme['yellow-dark']};
+  
+  font-size: 32px;
+  font-weight: bolder;
+  margin-bottom: 4px;
+  `
+
+
+export const SubTitle = styled.h1`
+  color: ${({ theme }) => theme['base-subtitle']};
+  
+  font-size: 20px;
+  font-weight: 400;
+  margin-bottom: 40px;
+  `
+export const Card = styled.div`
+    padding: 2px;
+
+    border-radius: 6px 40px 6px 40px;
+    background: linear-gradient(90deg, ${({ theme }) => theme.yellow}, ${({ theme }) => theme.purple});
+  
+    > div {
+      background: #fafafa; 
+      border-radius: 6px 40px 6px 40px;
+      padding: 40px;
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+    }
+  `;
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  div:last-child {
+    display: flex;
+    flex-direction: column;
+  }
+
+  div span {
+    color: ${({theme}) => theme["base-text"]};
+  }
+`
+
+export const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 50%;
+
+  width: 32px;
+  height: 32px;
+`
