3b747934adbf6263094caa56c410b5565fd20530
Cleber Severo <clebersevero1998@gmail.com>
2025-09-10 20:46:22 -0300

    fix: type

--- src/@types/coffe.ts
+++ src/@types/coffe.ts
@@ -1,13 +1,13 @@
 export interface ICoffe {
-	title: string;
-	description: string;
-	price: string;
-	tags: string[];
-	url: string;
-	id: string;
+  title: string;
+  description: string;
+  price: string;
+  tags: string[];
+  url: string;
+  id: string;
   amount?: number;
 }
 
 export interface ICartItem extends ICoffe {
-	amount: number
+  amount: number;
 }
